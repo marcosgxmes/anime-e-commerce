9a0baee28653128d8ce313fe4914e99f2562ec15
Marcos Gomes <129970704+marcosgxmes@users.noreply.github.com>
2024-12-08 17:05:07 -0300

    update cart layout

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -25,62 +25,63 @@ export function Cart() {
       )}
 
       {cart.map((item) => (
-        <section
-          key={item.id}
-          className="flex justify-items-start w-full max-w-5xl border-b-2 mb-5 pb-1 border-gray-300 ">
+        <section key={item.id}
+          className="flex flex-col justify-items-start w-full max-w-5xl border mb-5 pb-1 border-gray-300 rounded-3xl ">
 
-          <div className='flex  justify-between gap-4 w-full '>
-            <Link to={`/product/${item.id}`}>
-              <img
-                src={item.cover}
-                alt={item.title}
-                className="w-28 mb-1"
-              />
-            </Link>
+          <div className='flex justify-between gap-4 w-full border-2 border-border rounded-3xl'>
+            <div className='flex itens-center justify-center w-4/6 border-2 border-black'>
+              <Link to={`/product/${item.id}`}>
+                <img
+                  src={item.cover}
+                  alt={item.title}
+                  className="w-28"
+                />
+              </Link>
+            </div>
 
 
             <div className='w-full flex flex-col items-start gap-3'>
-              <div >
+              <div>
                 <p className='font-bold text-lg'>{item.title}</p>
-              </div>
-
-              <div className='flex flex-col gap-1 text-sm'>
                 <p>Quantidade :</p>
-                <div className="flex items-center justify-center gap-2 border-solid border px-2 rounded-md font-roboto bg-white border-border">
-                  <button
-                    onClick={() => removeItemCart(item)}
-                    className="p-1 rounded-md font-medium flex item-center justify-center">
-                    <CiCircleMinus size={26} color='#333' />
-                  </button>
-
-                  {item.amount}
-
-                  <button
-                    onClick={() => addItemCart(item)}
-                    className="p-1 rounded-md text-white font-medium flex item-center justify-center">
-                    <BsPlusCircle size={22} color="#333" />
-                  </button>
-                </div>
               </div>
 
-              <div className='w-full flex items-center justify-between '>
-
+              <div className='w-full flex items-center justify-between border-t border-border pt-3'>
                 <strong className="float-right text-colorTotal text-lg font-roboto">
                   {item.total.toLocaleString("pt-BR", {
                     style: 'currency',
                     currency: "BRL"
                   })}
-                </strong>
-
-                <button
-                  className='p-1 flex items-center gap-1 text-colorTotal font-bold text-sm'
-                  onClick={() => deleteItemCart(item)}>     
-                  Remover             
-                  <BsTrash size={24} color='#004280' />                  
-                </button>
+                </strong>                
               </div>
+
             </div>
-          </div>    
+          </div>
+
+          <div className='flex items-center justify-between p-2 text-sm'>           
+            <div className="flex items-center justify-center gap-2 border-solid border px-2 rounded-md font-robot">
+              <button
+                onClick={() => removeItemCart(item)}
+                className="p-1 rounded-md font-medium flex item-center justify-center">
+                <CiCircleMinus size={26} color='#333'  />
+              </button>
+
+              {item.amount}
+
+              <button
+                onClick={() => addItemCart(item)}
+                className="p-1 rounded-md text-white font-medium flex item-center justify-center">
+                <BsPlusCircle size={22} color="#333" />
+              </button>
+            </div>
+
+            <button
+                  className='p-1 flex items-center gap-1 text-colorTotal font-bold text-sm'
+                  onClick={() => deleteItemCart(item)}>
+                  Remover
+                  <BsTrash size={24} color='#004280' />
+                </button>
+          </div>
         </section>
       ))}
 
