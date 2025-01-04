179225be2af0792be86a11ec92948192a018f85e
Marcos Gomes <129970704+marcosgxmes@users.noreply.github.com>
2025-01-04 17:01:52 -0300

    Update index.tsx

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,12 +1,10 @@
 import { Outlet } from 'react-router-dom'
-import { Header } from '../header'
 import { Footer } from '../footer'
 import { Cart } from '../cart'
 
 export function Layout(){
   return(
     <>
-      {/* <Header /> */}
       <Cart />
       <Outlet /> 
       <Footer />
