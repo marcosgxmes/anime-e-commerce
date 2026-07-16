bd77b8974a484f068e15e3a395ea9f7d17a2fffa
Marcos Gomes <marcosgms777@gmail.com>
2026-07-16 11:22:35 -0300

    update UI login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,110 +1,123 @@
-import { Link , useNavigate} from 'react-router-dom'
-import logoImg from '../../../public/dc_circle_black.png'
+import { Link, useNavigate } from "react-router-dom";
+import logoImg from "../../../public/dc_circle_black.png";
 
-import { useEffect } from 'react'
+import { useEffect } from "react";
 
-import { Input } from "../../components/input"
-import { useForm } from 'react-hook-form'
-import { z } from 'zod'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { Container } from '../../components/container'
+import { Input } from "../../components/input";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Container } from "../../components/container";
 
-import { signInWithEmailAndPassword, signOut}  from "firebase/auth"
-import { auth } from "../../services/api"
+import { signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { auth } from "../../services/api";
 
 const schema = z.object({
-  email: z.string().email("* Digite um email válido").nonempty("* O email é obrigatório"),
-  password: z.string().nonempty("* A senha é obrigatória")
-})
+	email: z
+		.string()
+		.email("* Digite um email válido")
+		.nonempty("* O email é obrigatório"),
+	password: z.string().nonempty("* A senha é obrigatória"),
+});
 
-type FormData = z.infer<typeof schema>
+type FormData = z.infer<typeof schema>;
 
 export function Login() {
-  const { register, handleSubmit, formState: { errors } } = useForm<FormData>({
-    resolver: zodResolver(schema),
-    mode: "onChange"
-  })
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<FormData>({
+		resolver: zodResolver(schema),
+		mode: "onChange",
+	});
 
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  useEffect(() => {
-    async function handleLogout() {
-      await signOut(auth)
-    }
+	useEffect(() => {
+		async function handleLogout() {
+			await signOut(auth);
+		}
 
-    handleLogout()
-  }, [] )
+		handleLogout();
+	}, []);
 
-  function onSubmit(data: FormData) {
-    console.log(data)
+	function onSubmit(data: FormData) {
+		console.log(data);
 
-    signInWithEmailAndPassword(auth, data.email, data.password)
-    .then(() => {
-      console.log("LOGADO COM SUCESSO")
-      navigate("/", { replace: true})
+		signInWithEmailAndPassword(auth, data.email, data.password)
+			.then(() => {
+				console.log("LOGADO COM SUCESSO");
+				navigate("/", { replace: true });
+			})
+			.catch((err) => {
+				console.log("ERRO AO LOGAR");
+				console.log(err);
+			});
+	}
 
-    })
-    .catch(err => {
-      console.log("ERRO AO LOGAR")
-      console.log(err)
-    })
-  }
+	return (
+		<Container>
+			<div className="min-h-screen  flex items-center justify-center md:p-4">
+				<main className="flex py-5 flex-col items-center justify-center max-w-2xl w-full bg-white md:rounded-2xl sm:shadow-xl overflow-hidden">
+					<Link
+						to="/"
+						className="mb-6 max-w-40 w-full h-34 flex items-center justify-center"
+					>
+						<img
+							src={logoImg}
+							alt="Logo do site"
+							className="object-contain w-full"
+						/>
+					</Link>
 
-  return (
-    <Container>
-      <div className='bg-white w-full min-h-screen flex justify-start items-center pt-36 flex-col gap-4'>
+					<h1 className="text-3xl font-medium text-texts">Bem vindo!</h1>
+					<p className="text-texts text-sm">
+						Faça Login ou crie uma conta para continuar
+					</p>
 
-        <Link to="/" className='mb-6 max-w-40 w-full h-34 flex items-center justify-center'>
-          <img
-            src={logoImg}
-            alt="Logo do site"
-            className='object-contain w-full'
-          />
-        </Link>
+					<form
+						onSubmit={handleSubmit(onSubmit)}
+						className="max-w-xl w-full rounded-lg px-4 mt-8 mb-4"
+					>
+						<div className="mb-3">
+							<Input
+								type="email"
+								placeholder="Email"
+								name="email"
+								error={errors.email?.message}
+								register={register}
+							/>
+						</div>
 
-        <h1 className='text-3xl font-medium text-texts'>Bem vindo!</h1>
-        <p className='text-texts text-sm'>Faça Login ou crie uma conta para continuar</p>
+						<div className="mb-6">
+							<Input
+								type="password"
+								placeholder="Senha"
+								name="password"
+								error={errors.password?.message}
+								register={register}
+							/>
+						</div>
 
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className='max-w-xl w-full rounded-lg px-4 pt-4 pb-2'
-        >
-          <div className='mb-3'>
-            <Input
-              type="email"
-              placeholder="Email"
-              name="email"
-              error={errors.email?.message}
-              register={register}
-            />
-          </div>
+						<button
+							className="bg-gradient-to-t from-purple to-cleanPurple hover:bg-none hover:bg-purple rounded-2xl w-full text-white h-10 mb-3 font-medium"
+							type="submit"
+						>
+							Entrar
+						</button>
 
-          <div className='mb-6'>
-            <Input
-              type="password"
-              placeholder="Senha"
-              name="password"
-              error={errors.password?.message}
-              register={register}
-            />
-          </div>
+						<button className="bg-gradient-to-t from-purple to-cleanPurple hover:bg-none hover:bg-purple rounded-2xl w-full text-white h-10 font-medium">
+							Esqueci minha senha
+						</button>
+					</form>
 
-          <button className='bg-gradient-to-t from-purple to-cleanPurple hover:bg-none hover:bg-purple rounded-2xl w-full text-white h-10 mb-3 font-medium' type='submit'>
-            Entrar
-          </button>
-
-          <button className='bg-purple hover:bg-none hover:bg-purple rounded-2xl w-full text-white h-10 font-medium'>
-            Esqueci minha senha
-          </button>
-
-        </form>
-
-        <Link to="/register" className='text-texts text-sm text-center mb-8'>
-          Não possui uma conta? <span className='text-purple'>Cadastre-se aqui!</span>
-        </Link>
-        
-      </div>
-    </Container>
-  )
+					<Link to="/register" className="text-texts text-sm text-center mb-4">
+						Não possui uma conta?{" "}
+						<span className="text-purple">Cadastre-se aqui!</span>
+					</Link>
+				</main>
+			</div>
+		</Container>
+	);
 }
-
